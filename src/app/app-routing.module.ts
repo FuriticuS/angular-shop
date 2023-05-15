8ee1b4513cfd94c48ddb8480f98d40655edaf6af
Fur1ouS <Fur1ouS@ya.ru>
2023-05-16 01:51:54 +0300

    create main cart product components + routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {MainLayoutComponent} from "./shared/main-layout/main-layout.component";
+import {MainPageComponent} from "./main-page/main-page.component";
+import {ProductPageComponent} from "./product-page/product-page.component";
+import {CartPageComponent} from "./cart-page/cart-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'', component: MainLayoutComponent, children:[
+      {path:'', redirectTo:'/', pathMatch: 'full'},
+      {path: '', component:MainPageComponent},
+      {path:'product/:id', component: ProductPageComponent},
+      {path:'cart', component: CartPageComponent}
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainLayoutComponent } from './shared/main-layout/main-layout.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { ProductPageComponent } from './product-page/product-page.component';
+import { CartPageComponent } from './cart-page/cart-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLayoutComponent,
+    MainPageComponent,
+    ProductPageComponent,
+    CartPageComponent
   ],
   imports: [
     BrowserModule,
