2c06b0978923093356dcda01c37a888bd678ba81
Fur1ouS <Fur1ouS@ya.ru>
2023-05-25 02:01:43 +0300

    create form for admin

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.scss']
 })
-export class LoginPageComponent {
+export class LoginPageComponent implements OnInit {
+  form: any | FormGroup;
+  submitted = false;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(6)]),
+    });
+  }
+
+  submit(){
+
+    this.submitted = true;
+
+    if (this.form.invalid) {
+      return;
+    }
+    console.log('Form get');
+  }
 
 }
